9974c87a8dd8b623edb444309ce9b1e9b01ec734
madhava mvs <sai.matta@bizcloudexperts.com>
2022-09-28 16:38:38 +0530

    added index

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import SalesDashboard from "./salesDashboard";
+// import HorizontalGraph from "./horizontalBarGraph";
+// import DoughnutChart from "./doughnut";
+// import VerticalGraph from "./verticalBarGraph";
+// import PopUp from "./popuppractice";
+// import Array from "./arraypractice"
+// import JsonArray from "./jsonpractice";
+import CheckList from "./checkList";
+// import TitleBar from './titlebar';
+// import Nav from './navigation';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <CheckList />
+  </React.StrictMode>
+);
+
